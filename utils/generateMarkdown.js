6dac34f1364bbc57150ee6bee0946db555a9c4db
Junioresc <junioresc1092@gmail.com>
2020-08-02 14:15:20 -0400

    done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,7 @@ function generateMarkdown(data) {
     return `
 # ${data.title}
 
-[![License](https://img.shields.io/badge/License-${data.license.split(" ", 0)}-blue)](LICENSE.txt)
+[![License](https://img.shields.io/badge/License-${data.license.split(" ", 1)}-blue)](LICENSE.txt)
 
 ## Description
 
