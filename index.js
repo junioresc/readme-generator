93f929fc0d10b882ea953f145e8941755e3e4bd0
Junioresc <junioresc1092@gmail.com>
2020-08-01 23:40:15 -0400

    added the markdown and questions for inquirer

--- index.js
+++ index.js
@@ -1,6 +1,100 @@
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: `input`,
+        name: `title`,
+        message: `What is the title of your project? (Required)`,
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log(`Please enter a project title!`);
+                return false;
+            }
+        }
+    },
+    {
+        type: `input`,
+        name: `github`,
+        message: `What is your GitHub username? (Required)`,
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log(`Please enter a GitHub username!`);
+                return false;
+            }
+        }
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `Please enter a contact email: (Required)`,
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log(`Please enter a contact email!`);
+                return false;
+            }
+        }
+    },
+    {
+        type: `input`,
+        name: `description`,
+        message: `Provide a description of the project: (Required)`,
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log(`Please enter a description for the project!`);
+                return false;
+            }
+        }
+    },
+    {
+        type: `input`,
+        name: `install`,
+        message: `Would you like to add any steps to the Installation?`,
+        default: `npm i`
+    },
+    {
+        type: `input`,
+        name: `usage`,
+        message: `Provide some information on how to use your program and examples for use. Include screenshots as needed:`
+    },
+    {
+        type: `input`,
+        name: `credits`,
+        message: `List any collaborators, third-party assets, or tutorials you followed with links for the credits:`
+    },
+    {
+        type: `list`,
+        name: `license`,
+        message: `Please choose a license for your project:`,
+        choices: [`MIT License`, `Apache License 2.0`, `GNU GPLv3`, `The Unlicense`]
+    },
+    {
+        type: `confirm`,
+        name: `confirmContributing`,
+        message: `Do you want to add information about contributing?`,
+        default: false
+    },
+    {
+        type: `input`,
+        name: `contributing`,
+        message: `Provide steps for how other developers can contribute to your project:`,
+        when: ({ confirmContributing }) => confirmContributing
+    },
+    {
+        type: `input`,
+        name: `tests`,
+        message: `Provide examples on how to run tests on your application:`,
+        default: `npm test`
+    }
 ];
 
 // function to write README file
@@ -9,7 +103,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions)
+        .then(answers => console.log(answers))
+        .catch(err => console.log(err));
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,62 @@
+const generateContributing = contributingText => {
+    if (!contributingText) {
+        return ``;
+    }
+    return `
+    ${contributingText}
+    `
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `
+    # ${data.title}
+
+    https://img.shields.io/github/languages/top/${data.github}/${data.title}
+    https://img.shields.io/badge/License-${data.license}-blue
+
+    ## Description
+
+    ${data.description}
+
+    ## Table of Contents
+
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Credits](#credits)
+    * [Questions](#questions)
+    
+    ## Installation
+
+    ${data.install}
+
+    ## Usage
+    
+        ${data.usage}
+
+    ## License
+
+    Licensed under the [${data.license}](LICENSE.txt) license.
+
+    ## Contributing
+
+    ${generateContributing(data.contributing)}
+
+    ## Tests
+
+        ${data.tests}
+
+    ## Credits
+
+    ${data.credits}
+
+    ## Questions
+
+    [GitHub](https://github.com/${data.github}/)
+    If you have any additional questions and would like to reach me, you can at ${data.email}
   `;
   }
   
